bd1c02d33590b11a596baf18795e081981b68495
joaomanoelvalerio <joaomanoelval@gmail.com>
2026-03-10 12:08:39 -0300

    tela login finalizada, atentar a comentarios no registerts

--- front-end/src/app/authentication/register/register.ts
+++ front-end/src/app/authentication/register/register.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router'; 
+import { MudaPagina } from '../../services/muda-pagina';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
+  standalone: true,
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class RegisterComponent {
+  MudaPagina = inject(MudaPagina)
 
+  usuario = {
+    cpf: '',
+    nome: '',
+    email: '',
+    telefone: '',
+    cep: ''
+    /* Posteriormente validar o cep no front usando a API ViaCEP e 
+    completar campos como cidade, bairro e etc. */
+  }
+
+  onSubmit() {
+  console.log('Dados enviados:', this.usuario);
+  // Falta integrar com a API aqui ! ! !
+  }  
+
+  click(event: MouseEvent) {
+  this.MudaPagina.mudarPagina(event, '/login');
+  }
 }
