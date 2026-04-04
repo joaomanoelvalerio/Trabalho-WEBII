e3cb3078baff38571ae6416863a5eab2baf7846c
vinyyyB <vhbocon@ggmail.com>
2026-04-04 15:17:34 -0300

    feat (frontend): add new routes

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -6,13 +6,15 @@ import { ClientHomeComponent } from './features/client/client-home/client-home';
 import { authGuard } from './core/guards/auth-guard';
 import { noAuthGuard } from './features/authentication/guards/no.auth-guard';
 import { ClientNewRequest } from './features/client/client-new-request/client-new-request';
+import { SolicitationsListComponent } from './features/employee/solicitations-list/solicitations-list';
 
 export const routes: Routes = [
-  { path: 'login',              component: LoginComponent,       canActivate: [noAuthGuard] },
-  { path: 'register',           component: RegisterComponent,    canActivate: [noAuthGuard] },
-  { path: 'employee',           component: Employee,             canActivate: [authGuard]   },
-  { path: 'client',             component: ClientHomeComponent,  canActivate: [authGuard]   },
-  { path: 'client/new-request', component: ClientNewRequest,     canActivate: [authGuard]   },
-  { path: '',                   redirectTo: '/login',            pathMatch: 'full'          },
-  { path: '**',                 redirectTo: '/login'                                        },
+  { path: 'login',                  component: LoginComponent,             canActivate: [noAuthGuard] },
+  { path: 'register',               component: RegisterComponent,          canActivate: [noAuthGuard] },
+  { path: 'employee',               component: Employee,                   canActivate: [authGuard]   },
+  { path: 'employee/solicitations', component: SolicitationsListComponent, canActivate: [authGuard]   },
+  { path: 'client',                 component: ClientHomeComponent,        canActivate: [authGuard]   },
+  { path: 'client/new-request',     component: ClientNewRequest,           canActivate: [authGuard]   },
+  { path: '',                       redirectTo: '/login',                  pathMatch: 'full'          },
+  { path: '**',                     redirectTo: '/login'                                              },
 ];
