4785c64988a24e166fd7e354557a1703c1e2ba16
Carlos-Vital <carlosknn2020@gmail.com>
2026-04-04 22:39:36 -0300

    adicionado relatórios de receitas(dia) e receitas por categoria em pdf.

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -7,12 +7,14 @@ import { authGuard } from './core/guards/auth-guard';
 import { noAuthGuard } from './features/authentication/guards/no.auth-guard';
 import { ClientNewRequest } from './features/client/client-new-request/client-new-request';
 import { SolicitationsListComponent } from './features/employee/solicitations-list/solicitations-list';
+import { RevenueReportsComponent } from './features/employee/revenue-reports/revenue-reports';
 
 export const routes: Routes = [
   { path: 'login',                  component: LoginComponent,             canActivate: [noAuthGuard] },
   { path: 'register',               component: RegisterComponent,          canActivate: [noAuthGuard] },
   { path: 'employee',               component: Employee,                   canActivate: [authGuard]   },
   { path: 'employee/solicitations', component: SolicitationsListComponent, canActivate: [authGuard]   },
+  { path: 'employee/reports',       component: RevenueReportsComponent,    canActivate: [authGuard]   },
   { path: 'client',                 component: ClientHomeComponent,        canActivate: [authGuard]   },
   { path: 'client/new-request',     component: ClientNewRequest,           canActivate: [authGuard]   },
   { path: '',                       redirectTo: '/login',                  pathMatch: 'full'          },
