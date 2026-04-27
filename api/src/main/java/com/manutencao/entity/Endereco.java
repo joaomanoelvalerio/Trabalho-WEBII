4e9f2afb46d290c16e9ae0759be1b4a310e4ff1a
joaomanoelvalerio <joaomanoelval@gmail.com>
2026-04-27 18:55:28 -0300

    endereco model added

---
+++ api/src/main/java/com/manutencao/entity/Endereco.java
@@ -0,0 +1,45 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+
+  @Column(name = "cep", nullable = false, length = 9)
+  @JsonProperty("cep")
+  private String cep;
+
+  @Column(name = "rua", nullable = false, length = 120)
+  @JsonProperty("rua")
+  private String rua;
+
+  @Column(name = "numero", nullable = false, length = 10)
+  @JsonProperty("numero")
+  private String numero;
+
+  @Column(name = "bairro", nullable = false, length = 80)
+  @JsonProperty("bairro")
+  private String bairro;
+
+  @Column(name = "cidade", nullable = false, length = 80)
+  @JsonProperty("cidade")
+  private String cidade;
+
+  @Column(name = "estado", nullable = false, length = 2)
+  @JsonProperty("estado")
+  private String estado;
+
+  @Column(name = "complemento")
+  @JsonProperty("complemento")
+  private String complemento;
+
+}
