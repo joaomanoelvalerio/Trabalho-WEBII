7e669a9522a6854cd32acc676a2bdb81920a5677
joaomanoelvalerio <joaomanoelval@gmail.com>
2026-05-11 17:03:47 -0300

    cpf fixed

--- front-end/src/app/features/authentication/register/register.ts
+++ front-end/src/app/features/authentication/register/register.ts
@@ -85,28 +85,17 @@ export class RegisterComponent {
     }
   }
 
-  formatCpf(event: Event) {
-    const input = event.target as HTMLInputElement;
-    let value = input.value.replace(/\D/g, '');
+  onCpfChange(value: string) {
+    this.user.cpf = this.formatCpfValue(value);
+  }
 
-    if (value.length > 11) {
-      value = value.substring(0, 11);
-    }
-
-    if (value.length > 9) {
-      value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})/, '$1.$2.$3-$4');
-
-    } else if (value.length > 6) {
-      value = value.replace(/(\d{3})(\d{3})(\d{1,3})/, '$1.$2.$3');
-
-    } else if (value.length > 3) {
-      value = value.replace(/(\d{3})(\d{1,3})/, '$1.$2');
-
-    }
-
-    input.value = value;
-
-    this.user.cpf = value;
+  formatCpfValue(value: string): string {
+    let clean = value.replace(/\D/g, '');
+    if (clean.length > 11) clean = clean.substring(0, 11);
+    if (clean.length <= 3) return clean;
+    if (clean.length <= 6) return clean.replace(/(\d{3})(\d{1,3})/, '$1.$2');
+    if (clean.length <= 9) return clean.replace(/(\d{3})(\d{3})(\d{1,3})/, '$1.$2.$3');
+    return clean.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})/, '$1.$2.$3-$4');
   }
 
   formatPhone(event: Event) {
