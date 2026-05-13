fb2156cd270d937570a7ce65e979b66382d70782
Carlos-Vital <carlosknn2020@gmail.com>
2026-05-13 20:03:39 -0300

    ajustado rotas no front-end.

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../app/shared/components/header/header';
-import { CategoryListComponent } from './component/category-list/category-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, CategoryListComponent],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
