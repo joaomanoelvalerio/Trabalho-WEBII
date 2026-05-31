6fc3f586943a228aa914441be2b0abac161f4126
vinyyyB <vhbocon@gmail.com>
2026-05-31 18:29:44 -0300

    add final details

--- front-end/src/app/core/guards/role-guard.ts
+++ front-end/src/app/core/guards/role-guard.ts
@@ -2,7 +2,6 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../features/authentication/services/auth.service';
 
-  // Permite apenas CLIENT
 export const clientGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
@@ -10,7 +9,6 @@ export const clientGuard: CanActivateFn = () => {
 
   if (user?.role === 'CLIENT') return true;
 
-  // Funcionário tentando acessar rota de cliente → redireciona para área dele
   if (user?.role === 'EMPLOYEE' || user?.role === 'ADMIN') {
     router.navigate(['/employee']);
     return false;
@@ -20,7 +18,6 @@ export const clientGuard: CanActivateFn = () => {
   return false;
 };
 
-  //Permite apenas EMPLOYEE ou ADMIN
 export const employeeGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
@@ -28,7 +25,6 @@ export const employeeGuard: CanActivateFn = () => {
 
   if (user?.role === 'EMPLOYEE' || user?.role === 'ADMIN') return true;
 
-  // Cliente tentando acessar rota de funcionário → redireciona para área dele
   if (user?.role === 'CLIENT') {
     router.navigate(['/client']);
     return false;
--- front-end/src/app/features/authentication/guards/auth-guard.ts
+++ front-end/src/app/features/authentication/guards/auth-guard.ts
@@ -1,6 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from '../../features/authentication/services/auth.service';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
